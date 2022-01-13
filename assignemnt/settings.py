6af835e00444ad6882d08908d3c30f8a8ba23479
ali raza <ali.raza@justads.com>
2022-01-13 19:59:52 +0500

    fix

--- assignemnt/settings.py
+++ assignemnt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0*$=%g=8bfi14h@-s)ff^*535rc6nmxmvdtmqn$5n$fmkh(q@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spekittestcode.herokuapp.com']
 
 
 # Application definition
@@ -83,13 +83,27 @@ WSGI_APPLICATION = 'assignemnt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spekit_db',
+        'USER': 'ali',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
